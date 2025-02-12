78842389bd0212287f6d819e278b1affc5c094ac
Ahmad Jerjees <ajerjees010@gmail.com>
2025-02-12 21:00:15 +0300

    test message

--- EmailDemo/Program.cs
+++ EmailDemo/Program.cs
@@ -10,23 +10,23 @@ var sender = new SmtpSender(() => new SmtpClient("smtp.gmail.com", 587)
 {
     EnableSsl = true,
     UseDefaultCredentials = false,
-    Credentials = new NetworkCredential("ahmad26.2.2002.jerjees@gmail.com", "My App Password")
-}); 
-    
-var emailBodyTemplate = new StringBuilder();
-emailBodyTemplate.AppendLine("Dear @Model.FirstName,");
-emailBodyTemplate.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it!<p>");
-emailBodyTemplate.AppendLine("-The JerCo Team");
+    Credentials = new NetworkCredential("ahmad26.2.2002.jerjees@gmail.com", "wrva evcr yfws wwve")
+});
+
+//var emailBodyTemplate = new StringBuilder();
+//emailBodyTemplate.AppendLine("Dear @Model.FirstName,");
+//emailBodyTemplate.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it!<p>");
+//emailBodyTemplate.AppendLine("-The JerCo Team");
 
 Email.DefaultSender = sender;
-Email.DefaultRenderer = new RazorRenderer();
+//Email.DefaultRenderer = new RazorRenderer();
 
 var email = await Email
     .From("ahmad26.2.2002.jerjees@gmail.com")
     .To("dutchjer99@gmail.com", "Dutch")
     .Subject("Thanks!")
-    .UsingTemplate(emailBodyTemplate.ToString(),new {FirstName = "Ahmad", ProductName = "test"})
-    //.Body("Thanks for buying my product.")
+    //.UsingTemplate(emailBodyTemplate.ToString(), new { FirstName = "Ahmad", ProductName = "test" })
+    .Body("Thanks for buying my product.")
     .SendAsync();
 
 if (email.Successful)
